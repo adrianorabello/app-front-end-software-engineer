fb9e78add998750f3dcfca8a4395d47c708f0d8a
adrianorabello <adrianorabello@icloud.com>
2026-02-03 23:45:03 +0000

    alteração

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -54,7 +54,7 @@ export default function App() {
       await new Promise(resolve => setTimeout(resolve, 800));
       
       //const response = await fetch('http://localhost:3000/ask');
-      const response = await fetch('http://52.4.148.215:3000/ask');
+      const response = await fetch('https://adrianorabello.com/ask');
       
       if (!response.ok) {
         throw new Error(`Server returned status: ${response.status}`);
